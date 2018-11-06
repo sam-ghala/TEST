119d41004e2975380515d4cb8d21217e86038c6c
Samuel Ghalayini <ghalayini.sj@gmail.com>
2018-11-06 16:34:29 -0700

    First commit

---
+++ Projects/CodeFights/codeTest.cpp
@@ -0,0 +1,21 @@
+//
+//Test
+// google code signal 
+//
+
+#include <iostream>
+#include <string>
+
+
+int main(){
+    int a[] = {2,1,3,5,3,2};
+    int len = (sizeof(a)/sizeof(a[0]))/2;
+    for(int k = 0; k < len*2; i++)
+        for (int i =0; i < len ;i++)
+        {
+            if(a[i] == )
+        }
+
+	return 0;
+}
+
---
+++ Projects/TEST/TEST.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Testing...\n"; 
+    
+    return 0;
+}
+
