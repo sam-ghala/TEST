342bd152a746bb53953b6b6374a0ad83f06dde5a
Samuel Ghalayini <ghalayini.sj@gmail.com>
2018-11-07 16:25:31 -0700

    Codesignal google example 1

--- Projects/CodeFights/codeTest.cpp
+++ Projects/CodeFights/codeTest.cpp
@@ -1,21 +1,27 @@
 //
 //Test
-// google code signal 
+// google code signal
 //
 
 #include <iostream>
 #include <string>
 
-
 int main(){
     int a[] = {2,1,3,5,3,2};
-    int len = (sizeof(a)/sizeof(a[0]))/2;
-    for(int k = 0; k < len*2; i++)
-        for (int i =0; i < len ;i++)
+    //int index = -1;
+    int value;
+    int len = (sizeof(a)/sizeof(a[0]));
+    for (int i =0; i < len/2 ;i++)
+    {
+        for(int k = 1; k < len;i++)
         {
-            if(a[i] == )
+          if(a[i] == a[k])
+              //index = k - i;
+              value = a[i];
         }
 
+    }
+    std::cout << value << "TESTING...\n";
+    //std::cin >> index;
 	return 0;
 }
-
