644c6ea6af6eb17ffdf9044fc31d075b8f29db47
Samuel Ghalayini <ghalayini.sj@gmail.com>
2018-11-10 13:09:53 -0700

    finished array 1

--- Projects/CodeFights/codeTest.cpp
+++ Projects/CodeFights/codeTest.cpp
@@ -5,31 +5,28 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 int main(){
-    int value, max, j;
+    int value, min, j;
     int arr[6] = {2,1,3,5,3,2};
     int len = sizeof(arr)/sizeof(arr[0]);
     int dumj[len];
-    int dumg[len];
     j = 0;
+    min = len;
     for (int i = 0; i < len/2;i++)
-    {
         for(int k = i+1; k < len;k++)
-        {
           if(arr[i] == arr[k])
+            if(min > (k-i))
+            {
+              min = (k-i);
               dumj[j] = arr[i];
-              dumg[j] = (k-i);
               j++;
+            }
         }
     }
-    max = dumg[0];
-    for(int i = 0; i < sizeof(dumj)/sizeof(dumj[0]);i++){
-        if(dumg[i] < max)
-            max = dumj[i];
-            std::cout << dumj[i] << " " << dumg[i] << std::endl;
-          }
-    std::cout << max << std::endl;
+      std::cout << *std::max_element(dumj,dumj + 4) << std::endl;
+
     std::getchar();
 	return 0;
 }
